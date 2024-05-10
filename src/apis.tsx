4142579e17ff942def4d6f7e3ff9e0e6af530cc1
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-05-10 14:28:46 +0530

    env import changed

--- src/apis.tsx
+++ src/apis.tsx
@@ -1,4 +1,6 @@
-const BASE_URL = process.env.BACKEND_URL;
+import process from "process";
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 import { DeviceDetailInterface, DeviceInfo } from "./interface";
 
