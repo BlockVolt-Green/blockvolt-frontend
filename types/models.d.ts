e7e50dd07be1f25ccbf9b31f01cdc356db792a27
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-05-27 07:31:29 +0530

    Models Added

---
+++ types/models.d.ts
@@ -0,0 +1,20 @@
+type User = {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+  machineAuthToken?: string | null;
+  devices: string[];
+};
+
+type Device = {
+  id: number;
+  address: string;
+  machineId: string;
+  userId: number;
+  country: string;
+  region: string;
+  city: string;
+  category: string;
+  manufacturer: string;
+};
