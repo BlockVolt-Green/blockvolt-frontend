87778518a95b2c6eb2fdae2641620ed0e5829284
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-05-27 07:22:48 +0530

    Minor Changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,6 @@ import { getDevices } from "@/apis";
 import { useEffect, useState } from "react";
 import { DeviceInfo } from "@/interface";
 import { useNavigate } from "react-router-dom";
-import MainLayout from "@/components/layouts/main-layout";
 import DashboardLayout from "@/components/layouts/dashboard-layout";
 
 export default function Home() {
@@ -34,10 +33,10 @@ export default function Home() {
     <DashboardLayout loading={false}>
       <div className="container">
         <div className="flex flex-wrap gap-4 justify-center p-10">
-          {devices.map((item) => {
+          {devices?.map((item) => {
             return (
               <Card
-                className="w-[300px] cursor-pointer"
+                className="w-[400px] cursor-pointer"
                 onClick={() =>
                   navigate(`/device-detail?address=${item.address}`)
                 }
@@ -46,15 +45,15 @@ export default function Home() {
                   <CardTitle>Device {item.id}</CardTitle>
                   <CardDescription></CardDescription>
                 </CardHeader>
-                <CardContent>
-                  <p className="text-sm text-left">
-                    <strong>Address: </strong> {item.address.slice(0, 20)}...
+
+                <CardContent className="text-md">
+                  <p className="">
+                    <strong>Address: </strong> {item.address}
                   </p>
-                  <p className="text-sm text-left">
-                    <strong>Machine Id: </strong> {item.machineId.slice(0, 16)}
-                    ...
+                  <p className="">
+                    <strong>Machine Id: </strong> {item.machineId}
                   </p>
-                  <p className="text-sm text-left">
+                  <p className="">
                     <strong>User ID: </strong> {item.userId}
                   </p>
                 </CardContent>
