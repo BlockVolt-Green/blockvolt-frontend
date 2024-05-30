779532dd5a95ace8d37aa02bd61995bc3f917847
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-05-30 20:49:39 +0530

    prompt no reg devices if empty

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -32,36 +32,43 @@ export default function DashboardPage() {
     <DashboardLayout loading={false}>
       <div className="container">
         <div className="flex flex-wrap gap-4 p-10">
-          {devices?.map((item) => {
-            return (
-              <Card
-                className="w-[400px] cursor-pointer"
-                onClick={() =>
-                  navigate(`/device-detail?address=${item.address}`)
-                }
-              >
-                <CardHeader>
-                  <CardTitle>{item.category.toUpperCase()} - {item.manufacturer.toUpperCase()}</CardTitle>
-                  <CardDescription></CardDescription>
-                </CardHeader>
 
-                <CardContent className="text-md">
-                  <p className="">
-                    <strong>Address: </strong> {item.address.slice(0,20)}...
-                  </p>
-                  <p className="">
-                    <strong>Machine Id: </strong> {item.machineId.slice(0,20)}...
-                  </p>
-                  <p className="">
-                    <strong>Location: </strong> {item.city.toUpperCase()}, {item.region.toUpperCase()} - {item.country}
-                  </p>
-                  <p className="">
-                    <strong>User ID: </strong> {item.userId}
-                  </p>
-                </CardContent>
-              </Card>
-            );
-          })}
+          {
+            devices.length === 0 ?
+            <p className="text-center w-full text-red-500 text-sm">No Registered Devices</p>
+            :
+            devices?.map((item) => {
+              return (
+                <Card
+                  className="w-[400px] cursor-pointer"
+                  onClick={() =>
+                    navigate(`/device-detail?address=${item.address}`)
+                  }
+                >
+                  <CardHeader>
+                    <CardTitle>{item.category.toUpperCase()} - {item.manufacturer.toUpperCase()}</CardTitle>
+                    <CardDescription></CardDescription>
+                  </CardHeader>
+  
+                  <CardContent className="text-md">
+                    <p className="">
+                      <strong>Address: </strong> {item.address.slice(0,20)}...
+                    </p>
+                    <p className="">
+                      <strong>Machine Id: </strong> {item.machineId.slice(0,20)}...
+                    </p>
+                    <p className="">
+                      <strong>Location: </strong> {item.city.toUpperCase()}, {item.region.toUpperCase()} - {item.country}
+                    </p>
+                    <p className="">
+                      <strong>User ID: </strong> {item.userId}
+                    </p>
+                  </CardContent>
+                </Card>
+              );
+            })
+          }
+
         </div>
       </div>
     </DashboardLayout>
